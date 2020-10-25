6de3b8e53cc5c8511dd4e290fb186a61c9bef14c
s17570 <s17570@pjwstk.edu.pl>
2020-10-25 11:18:47 +0100

    Zadanie 4

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string websiteAddress;
+
+            try
+            {
+                websiteAddress = args[0];
+            }
+            catch(Exception)
+            {
+                throw new ArgumentNullException();
+            }
+
+            // simple regex for website
+            var websiteAddressRegex = new Regex("https://[a-z.]+");
+            MatchCollection matchesWebsiteAddress = websiteAddressRegex.Matches(websiteAddress);
+
+            if(matchesWebsiteAddress.Count > 0)
+            {
+                var httpClient = new HttpClient();
+                HttpResponseMessage response;
+
+                try
+                {
+                    response = await httpClient.GetAsync(args[0]);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var html = await response.Content.ReadAsStringAsync();
+                        var emailRegex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                        var emailMatches = emailRegex.Matches(html);
+                        if (emailMatches.Count > 0)
+                        {
+                            var uniqueEmailMatches = emailMatches.OfType<Match>().Select(m => m.Value).Distinct();
+                            foreach (var i in uniqueEmailMatches)
+                            {
+                                Console.WriteLine(i);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nie znaleziono adresów email");
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Błąd w czasie pobierania strony");
+                }
+
+                httpClient.Dispose();
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
